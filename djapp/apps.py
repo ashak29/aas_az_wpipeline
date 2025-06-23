d44090953c6c83bf60e9d1c3556753f1f29d0326
Asha K <asha.kothandan@gmail.com>
2025-06-23 11:34:26 +0530

    added a files

--- djapp/apps.py
+++ djapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ApptestConfig(AppConfig):
+class DjappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apptest'
+    name = 'djapp'
