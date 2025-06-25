7c84edad7e548494eccf07c793076d6dbbce2251
Asha K <asha.kothandan@gmail.com>
2025-06-25 11:45:06 +0530

    update files

--- djapp/views.py
+++ djapp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
-def index(request):
-    print("Welcome to Azure app aservice with az_pipeline")
-    return HttpResponse("Hello from Django!")
+# def index(request):
+#     print("Welcome to Azure app aservice with az_pipeline")
+#     return HttpResponse("Hello from Django!")
